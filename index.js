bbc2226b401fd415052a28e9b0f895aa90eb8161
christian segura <ces1508@gmail.com>
2019-07-30 14:46:57 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require('express')
+const request = require('request')
+const API = 'https://lobubo.com/api/app'
+const DEFAULT = {
+  image: 'https://s3-us-west-2.amazonaws.com/lobubo/images/lobubo_face.jpg',
+  description: 'Atrae cientos de visitantes a tu negocio o emprendimiento',
+  name: 'Lobubo',
+  url: 'https://lobubo.com'
+}
+
+
+const app = express()
+app.set('view engine', 'pug')
+
+
+function buildPayload (data, url) {
+  let fullUrl = `${DEFAULT.url}/${url}`
+  if (data) {
+    return {
+      name: data.attributes.name,
+      image: data.attributes['image-data'].original.url || DEFAULT.image,
+      description: data.attributes.description,
+      price: data.attributes.price,
+      url: fullUrl
+    }
+  }
+  return { ...DEFAULT, fullUrl }
+}
+app.get('/ecommerce/product/:id', (req, res) => {
+  let { id } = req.params
+  request(`${API}/products/${id}`, (err, response, body) => {
+    if (err) {
+      return res.render('index', buildPayload(null, req.baseUrl))
+    }
+    let { data } = JSON.parse(body)
+    res.render('index', buildPayload(data, req.baseUrl))
+  })
+})
+app.get('/store/:id', (req, res) => {
+  let { id } = req.params
+  request(`${API}/brands/${id}`, (err, response, body) => {
+    if (err) {
+      return res.render('index', buildPayload(null, req.baseUrl))
+    }
+    let { data } = JSON.parse(body)
+    data.attributes['image-data'] = data.attributes.logo
+    res.render('index', buildPayload(data, req.baseUrl))
+  })
+})
+app.get('*', (req, res) => {
+  res.render('index', buildPayload(null, req.baseUrl))
+})
+app.listen(3101, err => {
+  if (err) {
+    console.log(`error launching server ${e.message}`)
+    throw err
+  }
+  console.log('server listening')
+})
+
