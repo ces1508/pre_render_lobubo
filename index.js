5929dd9ecdc4b7a361012d6fd6691b51f598e647
fdxhernandez <fdx-hernandez@hotmail.com>
2020-09-03 14:04:55 -0500

    bot integrado con campos seoTitle y SeoDescription desde backend

--- index.js
+++ index.js
@@ -1,31 +1,120 @@
 const express = require('express')
 const request = require('request')
-const API = 'https://dimo.app/api/app'
+const API = 'http://localhost:3000/api/app'
+const URL = 'https://dimo.app'
 const DEFAULT = {
   image: 'https://s3-us-west-2.amazonaws.com/lobubo/images/lobubo_face.jpg',
   description: 'Atrae cientos de visitantes a tu negocio o emprendimiento',
   name: 'Dimo',
   url: 'https://dimo.app'
 }
-
+const DefaultCoordinates = {
+  latitude: 4.708829,
+  longitude: -74.054164,
+  radius:1000
+}
 
 const app = express()
 app.set('view engine', 'pug')
 
 
 function buildPayload (data, url) {
-  let fullUrl = `${DEFAULT.url}${url}`
+  let fullUrl = `${URL}${url}`
   if (data) {
     return {
-      name: data.attributes.name,
+      name: data.attributes.seoTitle,
       image: data.attributes['image-data'].original.url || DEFAULT.image,
-      description: data.attributes.description,
-      price: data.attributes.price,
+      description: data.attributes.seoDescription,
       url: fullUrl
     }
   }
-  return { ...DEFAULT, fullUrl }
+  DEFAULT.url = fullUrl
+  return { ...DEFAULT }
 }
+
+app.get('/', (req, res) => {
+  DEFAULT.name = 'Dimo | Compra online moda, tecnologia, mascotas, alimentos y más.',
+  DEFAULT.description = 'Dimo tu centro comercial en línea, encuentra marcas 100% colombianas con enviós a todo Colombia. Encuentra moda, tecnología, restaurantes, supermecados.'
+  res.render('index', buildPayload(null, req.url))
+})
+app.get('/mobile', (req, res) => {
+  DEFAULT.name = 'Dimo | Compra desde tu celular moda, tecnologia, alimentos y más.'
+  DEFAULT.description = 'Dimo tu centro comercial en línea, encuentra marcas 100% colombianas con enviós a todo Colombia. Encuentra moda, tecnología, restaurantes, supermecados.'
+  res.render('index', buildPayload(null, req.url))
+})
+app.get('/productos', (req, res) => {
+  DEFAULT.name = 'Dimo.shop | Encuenta moda, tecnología, deportes, belleza y más.'
+  DEFAULT.description ='Dimo encuentra todo lo que estabas buscando en moda, tecnoloía, belleza, cuidado personal, accesorios de mascota y más, en una solo sitio web. Conoce más'
+  res.render('index', buildPayload(null, req.url))
+})
+app.get('/restaurantes', (req, res) => {
+  DEFAULT.name = 'Dimo.shop | Restaurantes a domicilio | Deliciosa comida a tu mesa'
+  DEFAULT.description = 'Te llevamos hasta la puerta de tu casa u oficna tu comida favorita en Dimo te recomendamos los mejores platos. Dimo.shop tu centro comercial en línea.'
+  res.render('index', buildPayload(null, req.url))
+})
+
+app.get('/restaurante/:brand/menu/:id', (req, res) => {
+  let { id } = req.params
+  request(`${API}/menus/${id}`, (err, response, body) => {
+    if (err) {
+      return res.render('index', buildPayload(null, req.url))
+    }
+    let { data } = JSON.parse(body)
+    DEFAULT.image = data.banner_img.url
+    DEFAULT.name = data.seo_title
+    DEFAULT.description = data.seo_description
+    DEFAULT.url = `${req.url}`
+    res.render('index', buildPayload(null, req.url))
+  })
+})
+app.get('/productos/categoria/:id', (req, res) => {
+  let { id } = req.params
+  request(`${API}/categories/${id}`, (err, response, body) => {
+    if (err) {
+      return res.render('index', buildPayload(null, req.url))
+    }
+    let { data } = JSON.parse(body)
+    DEFAULT.image = data.image.url
+    DEFAULT.name = data.seo_title
+    DEFAULT.description = data.seo_description
+    DEFAULT.url = `${req.url}`
+    res.render('index', buildPayload(null, req.url))
+  })
+})
+
+app.get('/restaurante/:id', (req, res) => {
+  let { id } = req.params
+  request(`${API}/brands/${id}?latitude=${DefaultCoordinates.latitude}&longitude=${DefaultCoordinates.longitude}&radius=${DefaultCoordinates.radius}`, (err, response, body) => {
+    if (err) {
+      return res.render('index', buildPayload(null, req.url))
+    }
+    let { data } = JSON.parse(body)
+    data.attributes['image-data'] = data.attributes.logo
+    res.render('index', buildPayload(data, req.url))
+  })
+})
+app.get('/tienda/:id', (req, res) => {
+  let { id } = req.params
+  request(`${API}/brands/${id}`, (err, response, body) => {
+    if (err) {
+      return res.render('index', buildPayload(null, req.url))
+    }
+    let { data } = JSON.parse(body)
+    data.attributes['image-data'] = data.attributes.logo
+    res.render('index', buildPayload(data, req.url))
+  })
+})
+app.get('/restaurante/:brand/comidas/:id', (req, res) => {
+  let { id } = req.params
+  request(`${API}/products/${id}`, (err, response, body) => {
+    if (err) {
+      return res.render('index', buildPayload(null, req.url))
+    }
+    let { data } = JSON.parse(body)
+    res.render('index', buildPayload(data, req.url))
+  })
+})
+
 app.get('/ecommerce/product/:id', (req, res) => {
   let { id } = req.params
   request(`${API}/products/${id}`, (err, response, body) => {
